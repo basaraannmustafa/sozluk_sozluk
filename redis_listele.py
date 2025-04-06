94d7b5267e1eddb0a6991ad1527629663b98ec03
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-06 14:56:37 +0300

    Kaydedilme hatası düzeltildi

--- redis_listele.py
+++ redis_listele.py
@@ -2,10 +2,10 @@ from redis_baglanti import redis_baglan
 
 def tum_kelimeleri_getir():
     r = redis_baglan()
-    keys = r.keys('*')
-    sozluk = {}
-    for key in keys:
-        sozluk[key] = r.get(key)
-    return dict(sorted(sozluk.items()))
+    kelimeler = {}
+    for key in r.keys('*'):
+        kelimeler[key] = r.get(key)
+    
+    sirali_kelimeler = dict(sorted(kelimeler.items()))
 
     return sirali_kelimeler
