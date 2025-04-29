241aad9d690f0559fc5a7ad3e28b732a07979fa1
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 01:57:35 +0300

    Update redis_listele.py
    Güncelleme yapıldı

--- redis_listele.py
+++ redis_listele.py
@@ -1,11 +1,18 @@
-from redis_baglanti import redis_baglan
+from redis_baglanti import redis_baglan 
 
 def tum_kelimeleri_getir():
     r = redis_baglan()
     kelimeler = {}
-    for key in r.keys('*'):
-        kelimeler[key] = r.get(key)
-    
-    sirali_kelimeler = dict(sorted(kelimeler.items()))
 
+    for key in r.keys('*'):
+        veri = r.hgetall(key)
+        # Redis'ten gelen veriler bytes türündedir, decode edelim
+        anlam = veri.get(b'anlam', b'').decode('utf-8')
+        es_anlam = veri.get(b'es_anlamlar', b'').decode('utf-8')
+        kelimeler[key.decode('utf-8')] = {
+            "anlam": anlam,
+            "es_anlamlar": es_anlam
+        }
+
+    sirali_kelimeler = dict(sorted(kelimeler.items()))
     return sirali_kelimeler
