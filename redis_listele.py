80639e1d66a76cbe65f0bac35270e79d2899e617
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 02:10:04 +0300

    Update redis_listele.py

--- redis_listele.py
+++ redis_listele.py
@@ -7,17 +7,17 @@ def tum_kelimeleri_getir():
     for key in r.keys('*'):
         if r.type(key) != b'hash':  # 👈 sadece hash türünde olanları al
             continue
-        
+            
         veri = r.hgetall(key)
-        # Redis'ten gelen veriler bytes türündedir, decode edelim
         anlam = veri.get(b'anlam', b'').decode('utf-8')
         es_anlam = veri.get(b'es_anlamlar', b'').decode('utf-8')
-        orijinal = veri.get(b'orijinal', key).decode('utf-8')  # 👈 işte burası
+        orijinal = veri.get(b'orijinal', key).decode('utf-8')
 
         kelimeler[orijinal] = {
             "anlam": anlam,
             "es_anlamlar": es_anlam
         }
+
 
     sirali_kelimeler = dict(sorted(kelimeler.items()))
     return sirali_kelimeler
