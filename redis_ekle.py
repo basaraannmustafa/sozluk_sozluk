9901a75b58cddd9652538a3c4e1488e78a0ad636
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 02:01:04 +0300

    -1Update redis_ekle.py

--- redis_ekle.py
+++ redis_ekle.py
@@ -1,13 +1,10 @@
-from redis_baglanti import redis_baglan
-
 def kelime_ekle(kelime, anlam, es_anlamlar=None):
     r = redis_baglan()
-    kelime_key = kelime.lower()
+    kelime_key = kelime.lower()  # Anahtar her zaman küçük
     veri = {
+        "orijinal": kelime,  # Kullanıcının girdiği gibi sakla
         "anlam": anlam.capitalize()
     }
-
     if es_anlamlar:
         veri["es_anlamlar"] = ", ".join(es_anlamlar)
-
     r.hset(kelime_key, mapping=veri)
