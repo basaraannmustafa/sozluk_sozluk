7d9db3e7d435ea3267f1e43bebdaa25fa71c47c4
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 02:04:03 +0300

    Update redis_sil.py

--- redis_sil.py
+++ redis_sil.py
@@ -2,4 +2,5 @@ from redis_baglanti import redis_baglan
 
 def kelime_sil(kelime):
     r = redis_baglan()
-    return r.delete(kelime.capitalize())  # Silerse 1 döner, yoksa 0
+    return r.delete(kelime.strip().lower())  # Tüm keyler küçük harfle tutuluyor
+
