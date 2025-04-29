9e363c82b4765de751d71c6fef6924faffa1b22b
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 01:55:48 +0300

    Update redis_ekle.py
    Yeni eklenti eklendi

--- redis_ekle.py
+++ redis_ekle.py
@@ -1,6 +1,13 @@
 from redis_baglanti import redis_baglan
 
-def kelime_ekle(kelime, anlam):
+def kelime_ekle(kelime, anlam, es_anlamlar=None):
     r = redis_baglan()
-    r.set(kelime.capitalize(),
-anlam.capitalize())
+    kelime_key = kelime.lower()
+    veri = {
+        "anlam": anlam.capitalize()
+    }
+
+    if es_anlamlar:
+        veri["es_anlamlar"] = ", ".join(es_anlamlar)
+
+    r.hset(kelime_key, mapping=veri)
