452b5b01720ab045c9152eab1174f74878f1c905
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-04-30 03:41:12 +0300

    Update redis_ekle.py

--- redis_ekle.py
+++ redis_ekle.py
@@ -2,16 +2,10 @@ from redis_baglanti import redis_baglan
 
 def kelime_ekle(kelime, anlam, es_anlamlar=None):
     r = redis_baglan()
-    if not r:
-        print("Redis bağlantısı kurulamadı.")
-        return
-
     kelime_key = kelime.strip().lower()
     veri = {
         "orijinal": kelime.strip(),
         "anlam": anlam.strip().capitalize(),
         "es_anlamlar": ", ".join(es_anlamlar or [])
     }
-
-    print(f"[DEBUG] Redis'e yazılıyor: {kelime_key} -> {veri}")
-    r
+    r.hset(kelime_key, mapping=veri)
